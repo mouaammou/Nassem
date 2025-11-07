1dd1959296f7ce989511c88dab1d392861a85f7a
mouaammou <jsamassine@gmail.com>
2025-11-07 12:24:54 +0100

    handle the error in the build time, the proxy is not middleware, im sure now

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { NextIntlClientProvider } from "next-intl";
-import { Baloo_Bhaijaan_2, Rubik, Lobster, Lobster_Two } from 'next/font/google'
+import { Baloo_Bhaijaan_2, Rubik, Lobster, Lobster_Two } from 'next/font/google';
+import { getMessages } from 'next-intl/server';
+
 
 const baloo = Baloo_Bhaijaan_2({
   subsets: ['latin'],
@@ -30,19 +31,24 @@ const lobsterTwo = Lobster_Two({
 })
 
 export const metadata: Metadata = {
-  title: "Multilingual App",
-  description: "Supports Arabic (RTL) and French (LTR)",
+  title: "Nassem - Smart Inhaler App",
+  description: "Transform your asthma management with our innovative smart inhaler application",
 };
-
 
 export default async function RootLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
+  // Await the params
   const { locale } = await params;
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
 
   // detect direction: Arabic = rtl, otherwise ltr
   const dir = locale === "ar" ? "rtl" : "ltr";
@@ -51,7 +57,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} dir={dir} className={`${baloo.variable} ${rubik.variable} ${lobster.variable} ${lobsterTwo.variable}`}>
       <body className={fontClass}>
-        <NextIntlClientProvider locale={locale}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
       </body>
